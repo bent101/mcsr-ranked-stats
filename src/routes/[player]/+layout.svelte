adda846d8bb77d2548c1088154230c9853453432
Ben Tomlin <bentomlin101@gmail.com>
2023-05-22 02:38:56 -0700

    add case for zero matches

--- src/routes/[player]/+layout.svelte
+++ src/routes/[player]/+layout.svelte
@@ -9,7 +9,7 @@
 	import Graph from "./Graph.svelte";
 
 	export let data;
-	let infiniteScrollPadding: HTMLElement;
+	let infiniteScrollPadding: HTMLElement | undefined;
 
 	const addMoreMatches = async () => {
 		if (data.noMoreMatches) return;
@@ -23,12 +23,14 @@
 	let io: IntersectionObserver;
 
 	onMount(() => {
-		io = new IntersectionObserver(([entry]) => {
-			if (entry.isIntersecting) {
-				addMoreMatches();
-			}
-		});
-		io.observe(infiniteScrollPadding);
+		if (infiniteScrollPadding) {
+			io = new IntersectionObserver(([entry]) => {
+				if (entry.isIntersecting) {
+					addMoreMatches();
+				}
+			});
+			io.observe(infiniteScrollPadding);
+		}
 	});
 
 	$: numMatches =
@@ -66,64 +68,68 @@
 					class="ml-2 rounded-full bg-zinc-400 px-2 py-0.5 text-xs font-extrabold text-zinc-900"
 					>{numMatches}</span>
 			</h2>
-			<ol class="relative pb-16">
-				{#each data.matches as { isDecay, opponent, outcome, forfeit, time, eloChange, date, id }}
-					{@const selected = $page.url.pathname === `/${data.playerData.nickname}/${id}`}
-					{@const color = outcome
-						? { won: "text-green-400", lost: "text-red-400", draw: "text-blue-400" }[outcome]
-						: "text-zinc-400"}
-					<li>
-						{#if isDecay}
-							<div
-								class="group flex items-center gap-2 rounded-lg px-4 py-1.5 hocus-within:bg-zinc-800">
-								<div class="w-40 italic text-zinc-500">Elo decay</div>
-								<div class="w-20 text-center text-sm font-bold uppercase text-red-400">
-									{eloChange >= 0 ? "+" : ""}{eloChange} elo
-								</div>
-								<div class="w-20" />
-								<div class=" w-12 text-right text-zinc-600">{date}</div>
-							</div>
-						{:else}
-							<a
-								data-sveltekit-replacestate
-								data-sveltekit-noscroll
-								href="/{data.playerData.nickname}/{id}"
-								class="group flex items-center gap-2 rounded-lg border px-4 py-1.5 text-left {selected
-									? 'border-zinc-500 bg-zinc-800'
-									: 'border-transparent hocus-within:bg-zinc-800'}">
+			{#if data.matches && data.matches.length > 0}
+				<ol class="relative pb-16">
+					{#each data.matches as { isDecay, opponent, outcome, forfeit, time, eloChange, date, id }}
+						{@const selected = $page.url.pathname === `/${data.playerData.nickname}/${id}`}
+						{@const color = outcome
+							? { won: "text-green-400", lost: "text-red-400", draw: "text-blue-400" }[outcome]
+							: "text-zinc-400"}
+						<li>
+							{#if isDecay}
 								<div
-									class="w-40 overflow-hidden overflow-ellipsis {selected
-										? 'text-zinc-50'
-										: 'text-zinc-300'}">
-									<a
-										data-sveltekit-replacestate="off"
-										data-sveltekit-noscroll="off"
-										href="/{opponent}"
-										class=" hover:underline hover:underline-offset-4">{opponent}</a>
+									class="group flex items-center gap-2 rounded-lg px-4 py-1.5 hocus-within:bg-zinc-800">
+									<div class="w-40 italic text-zinc-500">Elo decay</div>
+									<div class="w-20 text-center text-sm font-bold uppercase text-red-400">
+										{eloChange >= 0 ? "+" : ""}{eloChange} elo
+									</div>
+									<div class="w-20" />
+									<div class=" w-12 text-right text-zinc-600">{date}</div>
 								</div>
-								<div class="w-20 text-center uppercase {color} text-sm font-bold">
-									<span class="{selected ? ' hidden' : 'inline group-hocus-within:hidden'} "
-										>{outcome}</span>
-									<span class={selected ? " inline" : "hidden group-hocus-within:inline"}
-										>{eloChange >= 0 ? "+" : ""}{eloChange} elo</span>
-								</div>
-								<div
-									class="w-20 text-center font-extrabold tracking-wider {forfeit
-										? `text-xs font-semibold uppercase tracking-normal ${
-												selected ? 'text-zinc-300' : 'text-zinc-600'
-										  }`
-										: ''} {selected ? 'text-zinc-300' : 'text-zinc-500'} ">
-									{outcome === "draw" ? "" : forfeit ? "Forfeit" : time}
-								</div>
-								<div class="w-12 text-right text-zinc-{selected ? 50 : 600}">{date}</div>
-							</a>
-						{/if}
-					</li>
-				{/each}
-			</ol>
-			<div class="mb-[80vh] text-center text-zinc-600" bind:this={infiniteScrollPadding}>
-				{data.noMoreMatches ? "No more matches" : "Loading..."}
-			</div>
+							{:else}
+								<a
+									data-sveltekit-replacestate
+									data-sveltekit-noscroll
+									href="/{data.playerData.nickname}/{id}"
+									class="group flex items-center gap-2 rounded-lg border px-4 py-1.5 text-left {selected
+										? 'border-zinc-500 bg-zinc-800'
+										: 'border-transparent hocus-within:bg-zinc-800'}">
+									<div
+										class="w-40 overflow-hidden overflow-ellipsis {selected
+											? 'text-zinc-50'
+											: 'text-zinc-300'}">
+										<a
+											data-sveltekit-replacestate="off"
+											data-sveltekit-noscroll="off"
+											href="/{opponent}"
+											class=" hover:underline hover:underline-offset-4">{opponent}</a>
+									</div>
+									<div class="w-20 text-center uppercase {color} text-sm font-bold">
+										<span class="{selected ? ' hidden' : 'inline group-hocus-within:hidden'} "
+											>{outcome}</span>
+										<span class={selected ? " inline" : "hidden group-hocus-within:inline"}
+											>{eloChange >= 0 ? "+" : ""}{eloChange} elo</span>
+									</div>
+									<div
+										class="w-20 text-center font-extrabold tracking-wider {forfeit
+											? `text-xs font-semibold uppercase tracking-normal ${
+													selected ? 'text-zinc-300' : 'text-zinc-600'
+											  }`
+											: ''} {selected ? 'text-zinc-300' : 'text-zinc-500'} ">
+										{outcome === "draw" ? "" : forfeit ? "Forfeit" : time}
+									</div>
+									<div class="w-12 text-right text-zinc-{selected ? 50 : 600}">{date}</div>
+								</a>
+							{/if}
+						</li>
+					{/each}
+				</ol>
+				<div class="mb-[80vh] text-center text-zinc-600" bind:this={infiniteScrollPadding}>
+					{data.noMoreMatches ? "No more matches" : "Loading..."}
+				</div>
+			{:else}
+				<div class="w-[26.625rem] text-center text-zinc-600">No matches this season</div>
+			{/if}
 		</div>
 	</div>
 	<div class="sticky top-0 flex h-screen flex-1 flex-col gap-4 p-8 pl-0">
