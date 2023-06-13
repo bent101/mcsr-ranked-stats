afa7d0bbc9c151f10732af99402529ef760ea7f6
Ben Tomlin <bentomlin101@gmail.com>
2023-06-12 19:36:43 -0700

    maybe fix

--- src/lib/components/Graph.svelte
+++ src/lib/components/Graph.svelte
@@ -30,17 +30,19 @@
 </script>
 
 {#if browser}
-	<VisXYContainer {xDomain} {yDomain} width="100%" height="calc(max(24rem, 60vh))" {data}>
-		<VisArea color="#a1a1aa28" curveType="linear" {data} x={(d) => d.x} y={(d) => d.y} />
-		<VisLine color="#a1a1aa" curveType="linear" {data} x={(d) => d.x} y={(d) => d.y} />
+	<div class="h-full">
+		<VisXYContainer {xDomain} {yDomain} width="100%" height="calc(max(24rem, 60vh))" {data}>
+			<VisArea color="#a1a1aa28" curveType="linear" {data} x={(d) => d.x} y={(d) => d.y} />
+			<VisLine color="#a1a1aa" curveType="linear" {data} x={(d) => d.x} y={(d) => d.y} />
 
-		<VisAxis type="x" numTicks={5} label="Matches ago" />
-		<VisAxis type="y" numTicks={3} label="Elo" />
-	</VisXYContainer>
+			<VisAxis type="x" numTicks={5} label="Matches ago" />
+			<VisAxis type="y" numTicks={3} label="Elo" />
+		</VisXYContainer>
+	</div>
 {/if}
 
 <style>
-	:root {
+	.h-full {
 		--vis-axis-grid-color: #27272a;
 		--vis-axis-tick-color: #27272a;
 		--vis-axis-label-color: #71717a;
