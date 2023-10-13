a1f206302ecedf44b0e105497067a7198ce0fc59
Ben Tomlin <bentomlin101@gmail.com>
2023-10-12 20:57:53 -0700

    there

--- src/routes/lb/+layout.svelte
+++ src/routes/lb/+layout.svelte
@@ -14,13 +14,13 @@
 </svelte:head>
 
 <div class="mx-auto max-w-sm py-[70px] md:ml-16">
-	<h1 class="pt-[5.25rem] text-xl font-bold text-zinc-300">
+	<h1 class="pt-2 text-xl font-bold text-zinc-300">
 		Fastest times this season
 		<span class="ml-2 inline-block -translate-y-1">
 			<Switch dark options={["all", "unique"]} bind:onFirst={$showingAll} />
 		</span>
 	</h1>
-	<ol class="mt-5 border-t-2 border-zinc-800 pt-4">
+	<ol class="mt-8 border-t-2 border-zinc-800 pt-8">
 		{#each $showingAll ? data.allBestTimes : data.uniqueBestTimes as match, i (match.id)}
 			<li in:scale={{ delay: 100 + 8 * i, duration: 250, start: 0.7 }}>
 				<BestTimesTableRow place={i + 1} {match} />
