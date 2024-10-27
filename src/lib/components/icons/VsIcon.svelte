6f222cd7ded3ea99596c3f5d879ec1d0153a9a7b
Ben Tomlin <bentomlin101@gmail.com>
2024-10-26 21:52:42 -0700

    uh

--- src/lib/components/icons/VsIcon.svelte
+++ src/lib/components/icons/VsIcon.svelte
@@ -1,4 +1,8 @@
-<svg {...$$props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 25">
+<script lang="ts">
+  export let className: string;
+</script>
+
+<svg class={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 25">
   <text
     x="3"
     y="20"
