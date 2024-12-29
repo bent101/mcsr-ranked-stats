9ed2757b43aa967b6b7aadca9ff5a3b7b1d1d558
Ben Tomlin <bentomlin101@gmail.com>
2024-12-28 23:19:18 -0800

    -_-

--- src/lib/last-playoffs-season.ts
+++ src/lib/last-playoffs-season.ts
@@ -1,6 +1,8 @@
-import type { APIResponse, PlayoffsResponse } from "./ranked-api";
-import { getPlayoffsURL } from "./urls";
+// import type { APIResponse, PlayoffsResponse } from "./ranked-api";
+// import { getPlayoffsURL } from "./urls";
 
-export const lastPlayoffsSeason = await fetch(getPlayoffsURL())
-  .then((res) => res.json() as APIResponse<PlayoffsResponse>)
-  .then((res) => res.data.data.season);
+// export const lastPlayoffsSeason = await fetch(getPlayoffsURL())
+//   .then((res) => res.json() as APIResponse<PlayoffsResponse>)
+//   .then((res) => res.data.data.season);
+
+export const lastPlayoffsSeason = 5;
