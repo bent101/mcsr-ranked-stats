a3fdcfeca4060af3e7287a8ef37d0d99d43ed906
Ben Tomlin <bentomlin101@gmail.com>
2025-03-11 08:08:14 -0700

    add note

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,16 @@
 </svelte:head>
 
 <div class="grid h-screen place-items-center">
-  <div class="text-xl font-semibold text-zinc-500">
+  <div class="text-xl text-center font-semibold text-zinc-500">
     Click or search for a player to see their stats
+    <br />
+    <br />
+    <p class=" text-zinc-600 text-sm font-medium">
+      Note: This site has moved to <a
+        href="https://mcsrranked.com/stats"
+        class="underline underline-offset-2 decoration-zinc-700 text-zinc-500"
+        >mcsrranked.com/stats</a
+      > and will only receive updates there
+    </p>
   </div>
 </div>
